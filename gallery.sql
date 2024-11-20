e8f9de1f47be8538709ab1e8b8eaa079dc85fa59
uraharaINDO <149456275+OrangIndoAseli@users.noreply.github.com>
2024-11-20 12:22:30 +0700

    Add files via upload

--- gallery.sql
+++ gallery.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Okt 2024 pada 05.22
+-- Waktu pembuatan: 20 Nov 2024 pada 06.20
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -35,6 +35,17 @@ CREATE TABLE `album` (
   `UserID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `album`
+--
+
+INSERT INTO `album` (`AlbumID`, `NamaAlbum`, `Deskripsi`, `TanggalDibuat`, `UserID`) VALUES
+(20, 'ASEP SAEPULOH', 'ANAJAAJAJAs', '2024-11-05', 5),
+(23, 'KASEP', 'daodawmdwl', '2024-11-07', 4),
+(24, 'Monyet', 'kakak', '2024-11-19', 4),
+(25, 'mahmud', 'jjj', '2024-11-19', 10),
+(26, 'kipas', 'kipas adalah suatu benda yang ', '2024-11-19', 10);
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +62,18 @@ CREATE TABLE `foto` (
   `UserID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `foto`
+--
+
+INSERT INTO `foto` (`FotoID`, `JudulFoto`, `DeskripsiFoto`, `TanggalUnggah`, `LokasiFile`, `AlbumID`, `UserID`) VALUES
+(39, 'asep', 'asep', '2024-11-19', '../../assets/img/CILOK.jpg', 23, 4),
+(40, 'adadad', 'adadada', '2024-11-19', '../../assets/img/membuat.jpg', 20, 5),
+(41, 'edada', 'dadada', '2024-11-19', '../../assets/img/ROMI.png', 23, 4),
+(42, 'ftt', 'uyuy', '2024-11-19', '../../assets/img/dkomentasi2.jpg', 23, 4),
+(43, 'jg', 'hvh', '2024-11-19', '../../assets/img/49ff1661-3d50-4524-9d1e-83fbf95dc443.jpeg', 24, 4),
+(49, 'hged', 'ueof', '2024-11-19', '../../assets/img/wallpaperflare.com_wallpaper.jpg', 25, 10);
+
 -- --------------------------------------------------------
 
 --
@@ -65,6 +88,16 @@ CREATE TABLE `komentarfoto` (
   `TanggalKomentar` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `komentarfoto`
+--
+
+INSERT INTO `komentarfoto` (`KomentarID`, `FotoID`, `UserID`, `IsiKomentar`, `TanggalKomentar`) VALUES
+(18, 39, 5, 'laku semoga', '2024-11-19'),
+(19, 40, 4, 'sdd', '2024-11-19'),
+(21, 43, 10, 'knk', '2024-11-19'),
+(22, 42, 4, 'mememk\r\n', '2024-11-20');
+
 -- --------------------------------------------------------
 
 --
@@ -77,6 +110,23 @@ CREATE TABLE `likefoto` (
   `UserID` int(11) DEFAULT NULL,
   `TanggalLike` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `likefoto`
+--
+
+INSERT INTO `likefoto` (`LikeID`, `FotoID`, `UserID`, `TanggalLike`) VALUES
+(52, 39, 5, '2024-11-19'),
+(53, 40, 5, '2024-11-19'),
+(54, 40, 4, '2024-11-19'),
+(75, 42, 10, '2024-11-19'),
+(76, 41, 10, '2024-11-19'),
+(80, 40, 10, '2024-11-19'),
+(81, 39, 10, '2024-11-19'),
+(85, 43, 10, '2024-11-19'),
+(88, 43, 4, '2024-11-20'),
+(89, 49, 4, '2024-11-20'),
+(92, 42, 4, '2024-11-20');
 
 -- --------------------------------------------------------
 
@@ -92,6 +142,18 @@ CREATE TABLE `user` (
   `NamaLengkap` varchar(255) DEFAULT NULL,
   `Alamat` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`UserID`, `Username`, `Password`, `Email`, `NamaLengkap`, `Alamat`) VALUES
+(3, 'ilmannar', '$2y$10$szmtS4tcy0QaZcXno/bNLOm7Qeyq/8lWTMiQRCcJWgL.Dg6yFDz9m', 'daadadaa@gmail.com', 'yatim', 'Jalan Sangkuriang'),
+(4, 'romo', '$2y$10$c4V3MSnp4eUcmKnkEuQjrO7qP3/0uHP26r/yuxY1qme/amnjd8NhG', 'daadadaa@gmail.com', 'ASEP SAEPULOH', 'Jalan Sangkuriang'),
+(5, 'admin', '$2y$10$A/tAd37H3VAZfFOVlS3Vced9CyWhAJb5Bg2qvZYVCUysMBA3vvY82', 'admin@gamil.com', 'dudung', 'saeppp'),
+(8, 'kokoko', '$2y$10$eeKX1mK/IpNwOb8.yEAwreg3cbM2xuZpPoCADLTiMOQwHVr7h4d1S', 'koko@gmail.com', 'koko', 'koko'),
+(9, 'koko', '$2y$10$yi2Q57y77loBdZXLo3VXo.ZvG1waXFhrXJM4EUEWWn7NOk.3DdjvC', 'koko@gmail.com', 'koko', 'koko'),
+(10, 'kokokoko', '$2y$10$OHCkfxsGifd5tipLVDKxku207xdGIVOAPsyyTbaHLTAfd1H63u1M.', 'koko@gmail.com', 'kokoko', 'kokko');
 
 --
 -- Indexes for dumped tables
@@ -117,16 +179,16 @@ ALTER TABLE `foto`
 --
 ALTER TABLE `komentarfoto`
   ADD PRIMARY KEY (`KomentarID`),
-  ADD KEY `FotoID` (`FotoID`),
-  ADD KEY `UserID` (`UserID`);
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `komentarfoto_ibfk_1` (`FotoID`);
 
 --
 -- Indeks untuk tabel `likefoto`
 --
 ALTER TABLE `likefoto`
   ADD PRIMARY KEY (`LikeID`),
-  ADD KEY `FotoID` (`FotoID`),
-  ADD KEY `UserID` (`UserID`);
+  ADD KEY `UserID` (`UserID`),
+  ADD KEY `likefoto_ibfk_1` (`FotoID`);
 
 --
 -- Indeks untuk tabel `user`
@@ -142,31 +204,31 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `album`
 --
 ALTER TABLE `album`
-  MODIFY `AlbumID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `AlbumID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT untuk tabel `foto`
 --
 ALTER TABLE `foto`
-  MODIFY `FotoID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `FotoID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT untuk tabel `komentarfoto`
 --
 ALTER TABLE `komentarfoto`
-  MODIFY `KomentarID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `KomentarID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT untuk tabel `likefoto`
 --
 ALTER TABLE `likefoto`
-  MODIFY `LikeID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `LikeID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -189,15 +251,15 @@ ALTER TABLE `foto`
 -- Ketidakleluasaan untuk tabel `komentarfoto`
 --
 ALTER TABLE `komentarfoto`
-  ADD CONSTRAINT `komentarfoto_ibfk_1` FOREIGN KEY (`FotoID`) REFERENCES `foto` (`FotoID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `komentarfoto_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `komentarfoto_ibfk_1` FOREIGN KEY (`FotoID`) REFERENCES `foto` (`FotoID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `komentarfoto_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`);
 
 --
 -- Ketidakleluasaan untuk tabel `likefoto`
 --
 ALTER TABLE `likefoto`
-  ADD CONSTRAINT `likefoto_ibfk_1` FOREIGN KEY (`FotoID`) REFERENCES `foto` (`FotoID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `likefoto_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `likefoto_ibfk_1` FOREIGN KEY (`FotoID`) REFERENCES `foto` (`FotoID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `likefoto_ibfk_2` FOREIGN KEY (`UserID`) REFERENCES `user` (`UserID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
